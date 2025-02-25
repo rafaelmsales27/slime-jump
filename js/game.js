9c2562d7cff452ff2cdce5686ab9689c60ca5fa9
Rafael Sales <rafaelmsales27@gmail.com>
2025-02-25 22:01:46 +0100

    add boundries and basic jump

--- js/game.js
+++ js/game.js
@@ -9,13 +9,13 @@ window.addEventListener('load', function () {
     constructor(game) {
       this.game = game;
       window.addEventListener('keydown', event => {
-        if((event.key === 'ArrowUp') && this.game.keys.indexOf(event.key) === -1) {
+        if ((event.key === 'ArrowUp') && this.game.keys.indexOf(event.key) === -1) {
           this.game.keys.push(event.key);
         }
       });
 
       window.addEventListener('keyup', event => {
-        if(this.game.keys.indexOf(event.key) > -1) {
+        if (this.game.keys.indexOf(event.key) > -1) {
           this.game.keys.splice(this.game.keys.indexOf(event.key), 1);
         }
       });
@@ -27,15 +27,28 @@ window.addEventListener('load', function () {
       this.game = game;
       this.width = 47 * 2;
       this.height = 32 * 2;
-      this.x = 20;
-      this.y = 200;
-      this.speedY = 1;
+      this.positionX = 20;
+      this.positionY = 200;
+      this.gravity = 1;
+      this.onGround = false;
     }
 
     update() {
-      this.y += this.speedY;
-      if(this.game.keys.includes('ArrowUp')) {
-        this.speedY = -10; 
+      this.positionY += this.gravity;
+
+      if (this.positionY > (canvas.height - this.height) - 100) {
+        this.positionY = canvas.height - this.height - 100;
+        this.velocityY = 0.0;
+        this.onGround = true;
+      }
+
+      if (this.positionY < 0) {
+        this.positionY = 0;
+        this.gravity = 1; // Reset gravity to fall down
+      }
+
+      if (this.game.keys.includes('ArrowUp')) {
+        this.gravity = -2;
         //TODO implement jump
         // rembember to make jump not floaty 
         // make game remember if player have pressed jump right before touching the ground 
@@ -47,7 +60,7 @@ window.addEventListener('load', function () {
 
     // "context" instead of "drawingContext" because we can draw the player on another canvas other than the main one
     draw(context) {
-      context.fillRect(this.x, this.y, this.width, this.height)
+      context.fillRect(this.positionX, this.positionY, this.width, this.height)
     }
   }
 
@@ -68,7 +81,7 @@ window.addEventListener('load', function () {
   }
 
   class Game {
-    constructor(width, height){
+    constructor(width, height) {
       this.width = width;
       this.height = height;
       this.player = new Player(this);
@@ -76,17 +89,17 @@ window.addEventListener('load', function () {
       this.keys = [];
     }
 
-    update(){
+    update() {
       this.player.update();
     }
 
-    draw(context){
+    draw(context) {
       this.player.draw(context);
     }
   }
 
   const game = new Game(canvas.width, canvas.height);
-  
+
   function animate() {
     drawingContext.clearRect(0, 0, canvas.width, canvas.height);
     game.update();
