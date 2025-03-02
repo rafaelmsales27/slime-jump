dc969b996cbfa211ba1c8201c415be48b450b15d
Rafael Sales <rafaelmsales27@gmail.com>
2025-03-02 01:53:11 +0100

    tune jump, initiate obstacle logic

--- js/game.js
+++ js/game.js
@@ -10,10 +10,13 @@ window.addEventListener('load', function () {
   const playerHeight = 50;
   let playerPosition = { x: 50, y: 350 };
   let playerVelocity = 0;
-  let gravity = 200;
-  let jumpStrength = 500;
+  let jumpStrength = 350;
   let isTouchingGround = false;
+
+  let gravity = 500;
   let keys = {};
+
+  let howManyJumps = 0;
 
   function inputHandler() {
     window.addEventListener('keydown', function (event) {
@@ -28,8 +31,14 @@ window.addEventListener('load', function () {
   const bottomLine = canvas.height - playerHeight;
 
   function updateState(deltaTime) {
-    // apply dynamics (gravity)
+    // apply dynamics
     deltaTime /= 1000; // Convert to seconds
+    // if (playerVelocity < 0) {
+    //   gravity = 200; 
+    // } else {
+    //   gravity = 500; // Falling
+    // }
+    
     playerPosition.y += playerVelocity * deltaTime + 0.5 * gravity * deltaTime * deltaTime;
     playerVelocity += gravity * deltaTime;
 
@@ -37,6 +46,7 @@ window.addEventListener('load', function () {
     if (playerPosition.y > (bottomLine)){
       playerPosition.y = bottomLine;
       isTouchingGround = true;
+      playerVelocity = 0;
     }
     // upper boundry
     if (playerPosition.y < 0){
@@ -56,8 +66,30 @@ window.addEventListener('load', function () {
     // pos += vel*t + 1/2*acc*t*t
     // vel += acc*t
     if(isTouchingGround) {
-      playerVelocity -= jumpStrength;
+      playerVelocity = -jumpStrength;
       isTouchingGround = false;
+      howManyJumps++;
+    }
+  }
+
+
+  // Obstacle configuration
+  const obstacleWidth = 50;
+  const obstacleHeight = 50;
+  let obstaclePosition = {x: canvas.width, y: canvas.height - obstacleHeight}
+  const obstableInterval = 1000;
+  const obstacleVelocity = -50;
+  let destroyObstacle = false;
+
+  function obstacle(deltaTime){
+    // generate obstacle
+
+    // update position
+    obstaclePosition.x -= obstacleVelocity * deltaTime;
+    
+    // make it disappear when touches left side (0,X)
+    if (obstaclePosition.x < 0) {
+      destroyObstacle = true;
     }
   }
 
@@ -65,12 +97,22 @@ window.addEventListener('load', function () {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.fillStyle = 'blue';
     context.fillRect(playerPosition.x, playerPosition.y, playerWidth, playerHeight)
+
+    if (!destroyObstacle) {
+      context.fillStyle = 'red';
+      context.fillRect(obstaclePosition.x, obstaclePosition.y, obstacleWidth, obstacleHeight);
+    }
+
     //! Debug purposes
     if (isTouchingGround) {
       context.fillStyle = "black";
       context.font = "14px Arial";
       context.fillText("On Ground", playerPosition.x, playerPosition.y - 10);
     }
+
+    //! Debug purposes
+    context.fillText("Vel", 400, 300);
+    context.fillText(Math.round(playerVelocity), 400, 315);
   }
 
   //! For debbuging purposes (Remove later)
@@ -107,11 +149,12 @@ window.addEventListener('load', function () {
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
     // reScaleScreen();
-    inputHandler();
+    
     updateState(deltaTime);
     drawElements();
     drawGrid();
     requestAnimationFrame(gameLoop); // Recursively call gameLoop
   }
+  inputHandler();
   gameLoop(0);
 })
